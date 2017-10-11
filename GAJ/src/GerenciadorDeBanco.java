d1b694162185f3341736073b0e6cb28f8f018c41
eliveltonalves <elivelttonalves@hotmail.com>
2017-10-11 16:32:13 -0300

    codigo do programa

---
+++ GAJ/src/GerenciadorDeBanco.java
@@ -0,0 +1,30 @@
+
+public class GerenciadorDeBanco {
+	boolean verificar = true;
+	public boolean cadastrarPessoaFisica(PessoaFisica pf) {
+		
+		return verificar;
+	}
+		
+		
+	public boolean cadastrarPessoaJuridica(PessoaJuridica pj) {
+			
+			return verificar;
+	}
+	
+	public boolean cadastrarAdvogado(Advogado ad) {
+		
+		return verificar;
+	}
+	
+	public boolean cadastrarProcesso(Processo processo) {
+		
+		return verificar;
+	}
+	public boolean cadastrarPUsuario(Usuario us) {
+		
+		return verificar;
+	}
+		
+
+}
